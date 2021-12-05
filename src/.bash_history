0e199e936e447e75c5111e68a2a4691bbe625022
zackery <deversz@allegheny.edu>
2021-12-05 17:29:48 -0500

    90% finished code

---
+++ src/.bash_history
@@ -0,0 +1,37 @@
+ls
+gcc calculator.c -o calculator.out
+./calculator.out 
+exit
+ls
+gcc calculator.c -o calculator.out
+gcc calculator.c -o calculator.out
+gcc calculator.c -o calculator.out
+gcc calculator.c -o calculator.out
+./ calculator.out
+./calculator.out
+gcc calculator.c -o calculator.out
+gcc calculator.c -o calculator.out
+gcc calculator.c -o calculator.out
+gcc calculator.c -o calculator.out
+./calculator.out 
+gcc calculator.c -o calculator.out
+gcc calculator.c -o calculator.out
+./calculator.out 
+gcc calculator.c -o calculator.out
+./calculator.out 
+gcc calculator.c -o calculator.out
+./calculator.out 
+./calculator.out 
+./calculator.out 
+gcc calculator.c -o calculator.out
+./calculator.out 
+gcc calculator.c -o calculator.out
+./calculator.out 
+gcc calculator.c -o calculator.out
+./calculator.out 
+./calculator.out 
+gcc calculator.c -o calculator.out
+gcc calculator.c -o calculator.out
+./calculator.out 
+./calculator.out 
+exit
---
+++ src/calculator.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+int main() {
+  char op;
+  double first, second;
+  long double result = 1.0;
+  printf("Enter an operator (+, -, *, /, ^, &): ");
+  scanf("%c", &op);
+  printf("Enter two operands: ");
+  scanf("%lf %lf", &first, &second);
+
+  switch (op) {
+    case '+':
+      printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
+      break;
+    case '-':
+      printf("%.1lf - %.1lf = %.1lf", first, second, first - second);
+      break;
+    case '*':
+      printf("%.1lf * %.1lf = %.1lf", first, second, first * second);
+      break;
+    case '/':
+      printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
+      break;
+    case '^':
+      while (second != 0) {
+        result *= first;
+        --second;
+      }
+      printf("Answer = %.0Lf", result);
+      break;
+    case '&':
+    while (first != 0) {
+      second /= result;
+      --first;
+    }
+      printf("Answer = %.0Lf", result);
+      break;
+    // operator doesn't match any case constant
+    default:
+      printf("Error! operator is not correct");
+  }
+
+  return 0;
+}
